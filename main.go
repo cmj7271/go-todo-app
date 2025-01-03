c9448b6acdd0d2147c715b81ecf680ae22abdc6d
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-03 17:19:09 +0900

    56/port number
    함수 외부에서 port number 를 지정할 수 있도록 함수 시그니쳐 변경

--- main.go
+++ main.go
@@ -6,17 +6,29 @@ import (
 	"fmt"
 	"golang.org/x/sync/errgroup"
 	"log"
+	"net"
 	"net/http"
+	"os"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v\n", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 	}
 }
 
-func run(ctx context.Context) error {
-	s := &http.Server{Addr: ":18080",
+func run(ctx context.Context, l net.Listener) error {
+	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -24,7 +36,7 @@ func run(ctx context.Context) error {
 
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 			!errors.Is(err, http.ErrServerClosed) {
 			log.Printf("failed to close: %+v", err)
 			return err
