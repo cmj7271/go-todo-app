57edfa1866c2294fcfebb250e00268b7f5d5bc17
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-20 01:42:54 +0900

    63/mux 추가

---
+++ mux_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewMux(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/health", nil)
+	sut := newMux()
+
+	sut.ServeHTTP(w, r)
+	resp := w.Result()
+	t.Cleanup(func() { _ = resp.Body.Close() })
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("got status code %d, want %d", resp.StatusCode, http.StatusOK)
+	}
+
+	got, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed to read body: %v", err)
+	}
+
+	want := `{"status": "ok"}`
+	if string(got) != want {
+		t.Errorf("got body %q, want %q", string(got), want)
+	}
+}
