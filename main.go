f9c725b17d5c65dfc2a49cda33015f916ce2c517
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-19 20:35:09 +0900

    60/config 설정 및 main, compose 에 적용

--- main.go
+++ main.go
@@ -4,30 +4,34 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/cmj7271/go-todo-app/config"
 	"golang.org/x/sync/errgroup"
 	"log"
 	"net"
 	"net/http"
-	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v\n", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 	}
 }
 
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+		return err
+	}
+
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v\n", url)
+
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
